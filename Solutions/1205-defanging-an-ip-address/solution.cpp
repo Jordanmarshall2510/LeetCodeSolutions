78c58097c1028e5e205dd28069696b345d1943d2
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2025-01-29 15:06:57 +0000

    [LeetCode Sync] Runtime - 2 ms (51.65%), Memory - 7.7 MB (76.99%)

---
+++ Solutions/1205-defanging-an-ip-address/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string defanged = "";
+
+        for(size_t i = 0; i < address.size(); i++)
+        {
+            if('.' == address[i])
+            {
+                defanged += "[.]";
+            }
+            else
+            {
+                defanged += address[i];
+            }
+        }
+
+        return defanged;
+    }
+};
